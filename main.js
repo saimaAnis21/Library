6900c2115e88a8ee0b0350e98e25f000f8f4935b
saimaAnis21 <saima_rehman21@yahoo.com>
2021-04-02 12:30:41 +0500

    Add the display function in teh JS file

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function Book(title,author,pgs) {
   this.pgs=pgs;
   this.read=false;
 }
+
+function displayBooks(){
+    let txt="<table><tr><th>Title</th>";
+    txt= txt+"<th>Author</th><th>Pages</th><th>Read?</th><th>Delete?</th></tr>";
+        for(var i=0; i< myLibrary.length; i++){
+          txt=txt+"<tr><td>"+myLibrary[i].title+"</td>";
+          
+          txt=txt+"<td>"+myLibrary[i].author+"</td>";
+          
+          txt=txt+"<td>"+myLibrary[i].pgs+"</td>";
+          txt=txt+"<td><button onclick='changeRead("+i+")'>"+read(myLibrary[i].read)+"</button></td>";
+          txt=txt+"<td><button onclick='deleteBook("+i+")'>delete</button></td></tr>"
+          
+        }
+        txt=txt+"</table>";
+        document.getElementById("distbl").innerHTML= txt;
+    }
