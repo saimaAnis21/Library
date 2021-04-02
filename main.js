d4569c64a3305044c11c9dde3a3b4bf5f0607b81
saimaAnis21 <saima_rehman21@yahoo.com>
2021-04-02 12:39:16 +0500

    Add the function to add delete books

--- main.js
+++ main.js
@@ -7,6 +7,39 @@ function Book(title,author,pgs) {
   this.read=false;
 }
 
+function addBookToLibrary() {
+    const t= document.querySelector("#title").value
+    const a= document.querySelector("#author").value
+    const p = document.querySelector("#pgs").value
+   const addNewBook = new Book(t,a,p);
+    myLibrary.push(addNewBook);
+    displayBooks();
+ }
+ 
+ function deleteBook(index){
+ myLibrary.splice(index,1);
+ displayBooks();
+ }
+ 
+ function read(state){
+   if (state){
+     return "Read"
+   }
+   else{
+     return "Not Read"
+   }
+ }
+ 
+ function changeRead(i){
+   if (myLibrary[i].read === true){
+      myLibrary[i].read = false;
+   }
+   else{
+      myLibrary[i].read= true;
+   }
+   displayBooks()
+ }
+
 function displayBooks(){
     let txt="<table><tr><th>Title</th>";
     txt= txt+"<th>Author</th><th>Pages</th><th>Read?</th><th>Delete?</th></tr>";
