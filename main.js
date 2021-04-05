5ec9c9b7f607269ef48d2923046a2b04cefeeb43
saimaAnis21 <saima_rehman21@yahoo.com>
2021-04-05 16:22:29 +0500

    refactor library

--- main.js
+++ main.js
@@ -1,61 +1,68 @@
-const myLibrary = [];
 
-function Book(title, author, pgs) {
-  this.title = title;
-  this.author = author;
-  this.pgs = pgs;
-  this.read = false;
-}
-
-function read(state) {
-  if (state) {
-    return 'Read';
-  }
-  return 'Not Read';
-}
-
-function displayBooks() {
-  let txt = '<table><tr><th>Title</th>';
-  txt = `${txt}<th>Author</th><th>Pages</th><th>Read?</th><th>Delete?</th></tr>`;
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    txt = `${txt}<tr><td>${myLibrary[i].title}</td>`;
-
-    txt = `${txt}<td>${myLibrary[i].author}</td>`;
-
-    txt = `${txt}<td>${myLibrary[i].pgs}</td>`;
-    txt = `${txt}<td><button onclick='changeRead(${i})'>${read(myLibrary[i].read)}</button></td>`;
-    txt = `${txt}<td><button onclick='deleteBook(${i})'>delete</button></td></tr>`;
-  }
-  txt += '</table>';
-  document.getElementById('distbl').innerHTML = txt;
-}
+const Book = (title, author, pgs) => {
+  const read = false;
+  return {
+    title, author, pgs, read,
+  };
+};
 
 /* eslint-disable */
-function addBookToLibrary() {
-  const t = document.querySelector('#title').value;
-  const a = document.querySelector('#author').value;
-  const p = document.querySelector('#pgs').value;
-  const addNewBook = new Book(t, a, p);
-  myLibrary.push(addNewBook);
-  displayBooks();
-}
+const myLibrary = (() => {
+  const library = [];
 
-function deleteBook(index) {
-  myLibrary.splice(index, 1);
-  displayBooks();
-}
+  const read = (state) => {
+    if (state) {
+      return 'Read';
+    }
+    return 'Not Read';
+  };
+
+  const displayBooks = () => {
+    let txt = '<table><tr><th>Title</th>';
+    txt = `${txt}<th>Author</th><th>Pages</th><th>Read?</th><th>Delete?</th></tr>`;
+    for (let i = 0; i < library.length; i += 1) {
+      txt = `${txt}<tr><td>${library[i].title}</td>`;
+
+      txt = `${txt}<td>${library[i].author}</td>`;
+
+      txt = `${txt}<td>${library[i].pgs}</td>`;
+      txt = `${txt}<td><button onclick='myLibrary.changeRead(${i})'>${read(library[i].read)}</button></td>`;
+      txt = `${txt}<td><button onclick='myLibrary.deleteBook(${i})'>delete</button></td></tr>`;
+    }
+    txt += '</table>';
+    document.getElementById('distbl').innerHTML = txt;
+  };
+
+  const addBookToLibrary = () => {
+    const t = document.querySelector('#title').value;
+    const a = document.querySelector('#author').value;
+    const p = document.querySelector('#pgs').value;
+    const newBook = Book(t, a, p);
+    library.push(newBook);
+    displayBooks();
+  };
 
 
-function changeRead(i) {
-  if (myLibrary[i].read === true) {
-    myLibrary[i].read = false;
-  } else {
-    myLibrary[i].read = true;
-  }
+  const deleteBook = (index) => {
+    library.splice(index, 1);
+    displayBooks();
+  };
 
-  displayBooks();
-}
+  const changeRead = (i) => {
+    if (library[i].read === true) {
+      library[i].read = false;
+    } else {
+      library[i].read = true;
+    }
 
+    displayBooks();
+  };
+
+
+  return {
+    addBookToLibrary, displayBooks, changeRead, deleteBook,
+  };
+})();
 
 function changedisplay() {
   document.getElementById('frm1').style.display = 'block';
